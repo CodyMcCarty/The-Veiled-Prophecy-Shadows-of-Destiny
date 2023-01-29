598a339fa8dfa4165c48b69e4a93a5f4e914a6e1
Cody McCarty <codyjmccarty@gmail.com>
2023-01-29 15:31:38 -0600

    updated template to include window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <cstdio>
+#include "raylib.h"
+
 int main()
 {
-    printf("Hello World!");
+    const char *title = "My Game";
+    int viewHeight{720};
+    int viewWidth{1280};
+
+    InitWindow(viewWidth, viewHeight, title);
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        EndDrawing();
+    }
 }
