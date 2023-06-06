e8509b3b8dad8dbfbfb1a7050143c63814f5cc4f
Cody McCarty <codyjmccarty@gmail.com>
2023-06-06 15:57:59 -0500

    Loaded map

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 
 int main()
 {
-    const char *title = "My Game";
+    const char *title = "The Veiled Prophecy: Shadows of Destiny";
     int viewHeight{720};
     int viewWidth{1280};
 
     InitWindow(viewWidth, viewHeight, title);
     SetTargetFPS(60);
 
+    Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        Vector2 mapPos{0.0, 0.0};
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
         EndDrawing();
     }
+    CloseWindow();
 }
