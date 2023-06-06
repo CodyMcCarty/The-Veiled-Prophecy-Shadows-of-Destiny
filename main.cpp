2087274f36df72065d31167fdf82eee6ffde2579
Cody McCarty <codyjmccarty@gmail.com>
2023-06-06 16:53:33 -0500

    added keys

--- main.cpp
+++ main.cpp
@@ -10,13 +10,19 @@ int main()
     SetTargetFPS(60);
 
     Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
+    Vector2 mapPos{0.0, 0.0};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 mapPos{0.0, 0.0};
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y += 1.0;
+        if (IsKeyDown(KEY_S)) direction.y -= 1.0;
+
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         EndDrawing();
