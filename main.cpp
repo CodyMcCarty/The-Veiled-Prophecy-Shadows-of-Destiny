b6548a1d1f9ebfcd10bf048dfa8253216d44e069
Cody McCarty <codyjmccarty@gmail.com>
2023-06-06 23:26:22 -0500

    animated Knight

--- main.cpp
+++ main.cpp
@@ -16,12 +16,21 @@ int main()
   Vector2 MapPos{0.0, 0.0};
   float MoveSpeed{4.0};
   // TODO: make knight or a character struct or class
-  Texture2D Knight = LoadTexture("characters/knight_idle_spritesheet.png");
+  Texture2D KnightIdle = LoadTexture("characters/knight_idle_spritesheet.png");
+  Texture2D KnightRun = LoadTexture("characters/knight_run_spritesheet.png");
+  Texture2D Knight = KnightIdle;
   float KnightScale{4.0};
   Vector2 KnightPos{
       ((float)ViewWidth / 2.0f) -
           KnightScale * (0.5f * (float)Knight.width / 6.0f),
       ((float)ViewHeight / 2.0f) - KnightScale * (.05f * (float)Knight.height)};
+  // 1:facing right -1:facing left
+  float RightLeft{1.f};  // TODO: make it a bool?
+  // animation vars
+  float RunningTime{};
+  int Frame{};
+  const int MaxFrames{6};
+  const float UpdateTime{1.f / 12.f};
 
   while (!WindowShouldClose())
   {
@@ -38,13 +47,29 @@ int main()
     {
       MapPos = Vector2Subtract(
           MapPos, Vector2Scale(Vector2Normalize(direction), MoveSpeed));
+      RightLeft = direction.x < 0.f ? -1.f : 1.f;
+      Knight = KnightRun;
+    }
+    else
+    {
+      Knight = KnightIdle;
     }
 
     // Draw Map
     DrawTextureEx(Map, MapPos, 0.0, 4.0, WHITE);
 
+    // Update animation frame
+    RunningTime += GetFrameTime();
+    if (RunningTime >= UpdateTime)
+    {
+      Frame++;
+      RunningTime = 0.f;
+      if (Frame > MaxFrames) Frame = 0;
+    }
+
     // Draw Knight
-    Rectangle knightSource{0.f, 0.f, (float)Knight.width / 6.f,
+    Rectangle knightSource{Frame * (float)Knight.width / 6.f, 0.f,
+                           RightLeft * (float)Knight.width / 6.f,
                            (float)Knight.height};
     Rectangle knightDest{KnightPos.x, KnightPos.y,
                          (float)Knight.width / 6.f * KnightScale,
