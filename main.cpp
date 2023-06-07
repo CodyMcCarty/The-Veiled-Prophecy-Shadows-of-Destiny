d0b380d6e11e6de1cc0d499313431a0cc1585a22
Cody McCarty <codyjmccarty@gmail.com>
2023-06-06 21:31:01 -0500

    added movement

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
+#include <iostream>
+
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
-    const char *title = "The Veiled Prophecy: Shadows of Destiny";
-    int viewHeight{720};
-    int viewWidth{1280};
+  const char *title = "The Veiled Prophecy: Shadows of Destiny";
+  int viewHeight{720};
+  int viewWidth{1280};
 
-    InitWindow(viewWidth, viewHeight, title);
-    SetTargetFPS(60);
+  InitWindow(viewWidth, viewHeight, title);
+  SetTargetFPS(60);
 
-    Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
-    Vector2 mapPos{0.0, 0.0};
+  Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
+  Vector2 mapPos{0.0, 0.0};
+  float moveSpeed{4.0};
 
-    while (!WindowShouldClose())
+  while (!WindowShouldClose())
+  {
+    BeginDrawing();
+    ClearBackground(WHITE);
+
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+    if (IsKeyDown(KEY_D)) direction.x += 1.0;
+    if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+    if (IsKeyDown(KEY_S)) direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
     {
-        BeginDrawing();
-        ClearBackground(WHITE);
-
-        Vector2 direction{};
-        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if (IsKeyDown(KEY_D)) direction.x += 1.0;
-        if (IsKeyDown(KEY_W)) direction.y += 1.0;
-        if (IsKeyDown(KEY_S)) direction.y -= 1.0;
-
-        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
-
-        EndDrawing();
+      mapPos = Vector2Subtract(
+          mapPos, Vector2Scale(Vector2Normalize(direction), moveSpeed));
     }
-    CloseWindow();
+
+    DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+    EndDrawing();
+  }
+  CloseWindow();
 }
